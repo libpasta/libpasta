356f99616028d5cc1aec35b1056e8c85ed4b1e3c
Sam Scott <sam.scott89@gmail.com>
2018-01-16 10:35:00 -0500

    Add flag to libpasta-capi test.

--- libpasta-capi/ctest/compile.sh
+++ libpasta-capi/ctest/compile.sh
@@ -3,4 +3,4 @@
 set -ex
 
 cargo build --release --manifest-path ../Cargo.toml
-g++ -DDEBUG -g -o test test.cpp -Wall -I../include -L../target/release -lpasta
+g++ -DDEBUG -std=c++11 -g -o test test.cpp -Wall -I../include -L../target/release -lpasta
